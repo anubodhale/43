b4bfeb54897678e5846dc038080f8de4a37e4438
anubodhale <tasgaon922670@gmail.com>
2025-05-22 11:22:28 +0530

    snow file.java

---
+++ snow file.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+   
+    public static void main(String[] args) {
+       
+        printGreeting();
+    }
+    
+    public static void printGreeting() {
+        System.out.println("Hello, World!");
+    }
+}
